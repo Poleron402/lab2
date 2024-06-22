b71a03341638b18cbbe2af4326bb3dde94e26189
Poleron402 <zhperon@gmail.com>
2024-06-22 14:15:04 -0700

    done

--- js/script.js
+++ js/script.js
@@ -7,16 +7,14 @@ function initializeGame(){
     //console.log(magicNumber)
     document.querySelector("#resetBtn").style.display = "none"
     document.querySelector("#guessBtn").style.display = "inline"
+    //resetting the color of feedback since we changed it to green/red
     document.querySelector("#feedback").style.color = "aliceblue"
     document.querySelector("#feedback").style.display = "none"
-    document.querySelector("#guessesPrompt").style.display = "none"
     document.querySelector("#guessesPrompt").focus()
     document.querySelector("#guesses").textContent = ""
     document.querySelector("#playerGuess").value = ""
     document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
-    let playerGuess = document.querySelector("#playerGuess")
-    playerGuess.focus()
-    
+    document.querySelector("#playerGuess").focus()
 }
 initializeGame()
 function gameOver(){
@@ -25,44 +23,46 @@ function gameOver(){
     
 }
 function checkGuess(){
-    let guess = document.querySelector("#playerGuess").value
+    let guess = parseInt(document.querySelector("#playerGuess").value)
     document.querySelector("#guessesPrompt").style.display = "inline"
     document.querySelector("#feedback").style.display = "inline"
-    
-    console.log("Player guess: "+ guess)
+    // erasing the content in input field so that it is easier to reenter a value
+    document.querySelector("#playerGuess").value = ""
+    // console.log("Player guess: "+ guess)
     let feedback = document.querySelector("#feedback")
     if (guess < 1 || guess > 99){
-        feedback.textContent = "Guess out of range."
-        document.querySelector("#playerGuess").value = ""
-        return
-    }else if (!/^\d*\d$/.test(guess)){
+        feedback.textContent = "Guess out of range!"   
+        feedback.style.color = "orange"  
+        // return
+        //I thought I'd add a validation that the string entered consists of digits
+    }else if (!/^\d*\d$/.test(guess) || guess==''){
         feedback.textContent = "Guess must be an integer."
-        document.querySelector("#playerGuess").value = ""
-        return
-    }
-
-    numberAttempts--
-    console.log("Attempts: "+numberAttempts)
-    if(guess==magicNumber){
-        feedback.textContent = "You guessed it! You won!"
-        feedback.style.color = "green"
-        wins++
-        document.querySelector("#winsAndLosses").innerHTML = `<span id="win">Wins: ${wins}</span>, <span id="loss">Losses: ${losses}</span>`
-        document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
-        gameOver()
+        feedback.style.color = "orange"  
+        // return
     }else{
-        document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
-        document.querySelector("#guesses").textContent+= guess+" "
-        if (numberAttempts == 0){
-            feedback.textContent = "You lost."
-            feedback.style.color = "red"
-            losses++
-            document.querySelector("#winsAndLosses").innerHTML =  `<span id="win">Wins: ${wins}</span>, <span id="loss">Losses: ${losses}</span>`
+        numberAttempts--
+        // console.log("Attempts: "+numberAttempts)
+        if(guess==magicNumber){
+            feedback.textContent = "You guessed it! You won!"
+            feedback.style.color = "green"
+            wins++
+            document.querySelector("#winsAndLosses").innerHTML = `<span id="win">Wins: ${wins}</span> <span id="loss">Losses: ${losses}</span>`
+            document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
             gameOver()
-        }else if (guess > magicNumber){
-            feedback.textContent = "Guess was high"
         }else{
-            feedback.textContent = "Guess was low"
+            document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
+            document.querySelector("#guesses").textContent+= guess+" "
+            if (numberAttempts == 0){
+                feedback.textContent = `You lost. The number was ${magicNumber}`
+                feedback.style.color = "red"
+                losses++
+                document.querySelector("#winsAndLosses").innerHTML =  `<span id="win">Wins: ${wins}</span> <span id="loss">Losses: ${losses}</span>`
+                gameOver()
+            }else if (guess > magicNumber){
+                feedback.textContent = "Guess was high"
+            }else{
+                feedback.textContent = "Guess was low"
+            }
         }
     }
 }
