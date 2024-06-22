3097d27346490ea07a24381009c9355b63f01610
Poleron402 <zhperon@gmail.com>
2024-06-22 13:18:20 -0700

    final walkthrough before done

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ let wins = 0, losses = 0
 function initializeGame(){
     numberAttempts = 7
     magicNumber = Math.floor(Math.random()*98)+1
-    console.log(magicNumber)
+    //console.log(magicNumber)
     document.querySelector("#resetBtn").style.display = "none"
     document.querySelector("#guessBtn").style.display = "inline"
+    document.querySelector("#feedback").style.color = "aliceblue"
     document.querySelector("#feedback").style.display = "none"
     document.querySelector("#guessesPrompt").style.display = "none"
     document.querySelector("#guessesPrompt").focus()
+    document.querySelector("#guesses").textContent = ""
     document.querySelector("#playerGuess").value = ""
-    document.querySelector("#guessesCount").innerHTML = `Guesses left: ${numberAttempts}`
+    document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
     let playerGuess = document.querySelector("#playerGuess")
     playerGuess.focus()
     
@@ -20,7 +22,7 @@ initializeGame()
 function gameOver(){
     document.querySelector("#guessBtn").style.display = "none"
     document.querySelector("#resetBtn").style.display = "inline"
-    document.querySelector("#feedback").style.color = "aliceblue"
+    
 }
 function checkGuess(){
     let guess = document.querySelector("#playerGuess").value
@@ -45,17 +47,17 @@ function checkGuess(){
         feedback.textContent = "You guessed it! You won!"
         feedback.style.color = "green"
         wins++
-        document.getElementById("winsAndLosses").innerHTML = `Wins: ${wins}, Losses: ${losses}`
-        document.querySelector("#guessesCount").innerHTML = `Guesses left: ${numberAttempts}`
+        document.querySelector("#winsAndLosses").innerHTML = `<span id="win">Wins: ${wins}</span>, <span id="loss">Losses: ${losses}</span>`
+        document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
         gameOver()
     }else{
-        document.querySelector("#guessesCount").innerHTML = `Guesses left: ${numberAttempts}`
+        document.querySelector("#guessesCount").textContent = `Guesses left: ${numberAttempts}`
         document.querySelector("#guesses").textContent+= guess+" "
         if (numberAttempts == 0){
             feedback.textContent = "You lost."
             feedback.style.color = "red"
             losses++
-            document.getElementById("winsAndLosses").innerHTML = `Wins: ${wins}, Losses: ${losses}`
+            document.querySelector("#winsAndLosses").innerHTML =  `<span id="win">Wins: ${wins}</span>, <span id="loss">Losses: ${losses}</span>`
             gameOver()
         }else if (guess > magicNumber){
             feedback.textContent = "Guess was high"
